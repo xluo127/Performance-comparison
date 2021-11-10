dd475094314906f0ec8746cea332e244ea6e264e
Xiaotian Luo <32707280+xluo127@users.noreply.github.com>
2021-11-10 20:50:58 +0800

    Add files via upload

---
+++ Python/PD_sorting.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+from random import randint
+import timeit
+
+x = [[randint(1, 101) for i in range(0, 5)] for j in range(0, 10**6)]
+df = pd.DataFrame(x, columns = ['x1', 'x2', 'x3', 'x4', 'x5'])
+df.astype('int32').dtypes
+all_t_1e6 = []
+
+t_start = timeit.default_timer()
+df.sort_values(by = ['x1'])
+t = timeit.default_timer() - t_start
+all_t_1e6.append(t)
+
+t_start = timeit.default_timer()
+df.sort_values(by = ['x1', 'x2'])
+t = timeit.default_timer() - t_start
+all_t_1e6.append(t)
+
+t_start = timeit.default_timer()
+df.sort_values(by = ['x1', 'x2', 'x3', 'x4', 'x5'])
+t = timeit.default_timer() - t_start
+all_t_1e6.append(t)
+
+print(all_t_1e6)
+
+x = [[randint(1, 101) for i in range(0, 5)] for j in range(0, 10**7)]
+df = pd.DataFrame(x, columns = ['x1', 'x2', 'x3', 'x4', 'x5'])
+df.astype('int32').dtypes
+all_t_1e7 = []
+
+t_start = timeit.default_timer()
+df.sort_values(by = ['x1'])
+t = timeit.default_timer() - t_start
+all_t_1e7.append(t)
+
+t_start = timeit.default_timer()
+df.sort_values(by = ['x1', 'x2'])
+t = timeit.default_timer() - t_start
+all_t_1e7.append(t)
+
+t_start = timeit.default_timer()
+df.sort_values(by = ['x1', 'x2', 'x3', 'x4', 'x5'])
+t = timeit.default_timer() - t_start
+all_t_1e7.append(t)
+
+print(all_t_1e7)
