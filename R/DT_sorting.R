76c0d90b78d362db4e35eef98793109456c0e7fc
Xiaotian Luo <32707280+xluo127@users.noreply.github.com>
2021-11-11 15:06:39 +0800

    Use system.time for data set with many rows

--- R/DT_sorting.R
+++ R/DT_sorting.R
@@ -9,11 +9,24 @@ t2 = summary(r)$mean
 r = microbenchmark(df[order(df[, 1], df[, 2], df[, 3], df[, 4], df[, 5]), ], unit = "s")
 t3 = summary(r)$mean
 r = microbenchmark(dt[order(x1)], unit = "s")
+  
 t4 = summary(r)$mean
 r = microbenchmark(dt[order(x1, x2)], unit = "s")
 t5 = summary(r)$mean
 r = microbenchmark(dt[order(x1, x2, x3, x4, x5)], unit = "s")
 t6 = summary(r)$mean
+
+data.frame(df_time = c(t1, t2, t3), dt_time = c(t4, t5, t6))
+}
+
+sort_time_large <- function(df, dt){
+t1 = system.time(df[order(df[, 1]), ])
+t2 = system.time(df[order(df[, 1], df[, 2]), ])
+t3 = system.time(df[order(df[, 1], df[, 2], df[, 3], df[, 4], df[, 5]), ])
+
+t4 = system.time(dt[order(x1)])
+t5 = system.time(dt[order(x1, x2)])
+t6 = system.time(dt[order(x1, x2, x3, x4, x5)])
 
 data.frame(df_time = c(t1[["elapsed"]], t2[["elapsed"]], t3[["elapsed"]]), dt_time = c(t4[["elapsed"]], t5[["elapsed"]], t6[["elapsed"]]))
 }
@@ -45,6 +58,8 @@ df = data.frame(x1 = sample.int(100, 6e7, replace = T),
                 x4 = sample.int(100, 6e7, replace = T),
                 x5 = sample.int(100, 6e7, replace = T))
 dt = data.table(df)
+
+sort_time_large(df, dt)
 
 dt = dtc
 microbenchmark(setkey(dt, x1), unit = "s")
